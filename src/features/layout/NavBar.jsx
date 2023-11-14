c828cbcb0bdbb32b7e5942aec091f1cc3e2eafd7
Lei Shi <me@leishi.io>
2023-11-14 18:49:22 +0100

    Fix navbar dropdown

--- src/features/layout/NavBar.jsx
+++ src/features/layout/NavBar.jsx
@@ -18,54 +18,56 @@ const NavBar = () => {
             <Icon icon="List" size={24} />
           </label>
         </div>
-        <div className="flex-1">
-        </div>
+        <div className="flex-1"></div>
         <div className="flex flex-1 justify-end px-2">
           <div className="flex items-stretch">
-            <div className="dropdown dropdown-end">
-              <label tabIndex={0} className="avatar btn btn-circle btn-ghost">
+            <details className="dropdown dropdown-end">
+              <summary className="avatar btn btn-circle btn-ghost">
                 <div className="w-10 rounded-full">
                   <img src="https://picsum.photos/80" />
                 </div>
-              </label>
-              <ul
-                tabIndex={0}
-                className="menu-compact menu dropdown-content z-[1] rounded-box mt-3 w-40 bg-base-100 p-2 shadow"
-              >
+              </summary>
+              <ul className="menu-compact menu dropdown-content rounded-box z-[1] mt-3 w-40 bg-base-100 p-2 shadow">
                 <li>
                   <a className="justify-between">
                     Profile
                     <span className="badge">New</span>
                   </a>
                 </li>
-                <li className="dropdown">
-                  <span tabIndex={1}>{t("Themes")}</span>
-                  <ul className="dropdown-content rounded-box -left-full top-px h-[70vh] max-h-96 w-40 overflow-y-auto bg-base-100 p-2 shadow">
-                    {themes.map((t) => (
-                      <li key={t}>
-                        <button
-                          key={t}
-                          className="overflow-hidden"
-                          onClick={() => setTheme(t)}
-                        >
-                          {t}
-                        </button>
-                      </li>
-                    ))}
-                  </ul>
+                <li>
+                  <details className="dropdown">
+                    <summary>{t("Themes")}</summary>
+                    <ul className="dropdown-content rounded-box -left-full top-px z-[2] h-[70vh] max-h-96 w-40 overflow-y-auto bg-base-100 p-2 shadow">
+                      {themes.map((t) => (
+                        <li key={t}>
+                          <button
+                            key={t}
+                            className="overflow-hidden"
+                            onClick={() => setTheme(t)}
+                          >
+                            {t}
+                          </button>
+                        </li>
+                      ))}
+                    </ul>
+                  </details>
                 </li>
-                <li className="dropdown">
-                  <span tabIndex={2}>{t("Language")}</span>
-                  <ul className="dropdown-content rounded-box -left-full w-40 bg-base-100 p-2 shadow">
-                    <li>
-                      <a onClick={() => i18n.changeLanguage("en")}>{t("EN")}</a>
-                    </li>
-                    <li>
-                      <a onClick={() => i18n.changeLanguage("zh")}>
-                        {t("中文")}
-                      </a>
-                    </li>
-                  </ul>
+                <li>
+                  <details className="dropdown">
+                    <summary>{t("Language")}</summary>
+                    <ul className="dropdown-content rounded-box -left-full z-[3] w-40 bg-base-100 p-2 shadow">
+                      <li>
+                        <a onClick={() => i18n.changeLanguage("en")}>
+                          {t("EN")}
+                        </a>
+                      </li>
+                      <li>
+                        <a onClick={() => i18n.changeLanguage("zh")}>
+                          {t("中文")}
+                        </a>
+                      </li>
+                    </ul>
+                  </details>
                 </li>
                 <li>
                   <a>Settings</a>
@@ -74,7 +76,7 @@ const NavBar = () => {
                   <a>Logout</a>
                 </li>
               </ul>
-            </div>
+            </details>
           </div>
         </div>
       </div>
